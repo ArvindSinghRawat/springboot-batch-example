3656697ec0c589d270184facba8a5311b377d955
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-08-03 18:27:11 +0530

    Added Sprinboot's boiler plate code

---
+++ src/test/java/com/asr/springboot/batch/SpringBootBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.springboot.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
